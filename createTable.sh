21497792fa80dc02dacde561ea3cdd26ac293589
omniagad25 <omniagad25@gmail.com>
2023-12-16 18:11:18 +0200

    create Table file

--- createTable.sh
+++ createTable.sh
@@ -7,7 +7,36 @@ if [ ! -f "$nameTable" ]; then
 
     case $nameTable in
          [a-zA-Z_]*)
-         touch $nameTable
+         nameTable=$(echo "databaseso/$nameTable" | tr ' ' '_')
+         ## when added the touch command here, if the user ended the program the file will be empty
+         
+         
+         count=0
+         dataType= true
+         conf= x
+         txt=""
+         txtType=""
+         read -p "Enter the number of columns: " col
+         while [ $count -le $col ]; do
+               while [ $conf != "Y" -a $conf != "y" ]; do
+                     read -p "Please write Y or y to confirm that you are going to write next the primary key nameTable" conf
+               done
+               read -p "Enter the name of column number $count: " colName
+               read -p "Enter the Type of column 'int' or 'string': " colType
+               if [ $count == 0 ]; then
+               txt=$colName":"
+               txtType=$colType":"
+               else
+               txt=$txt$colName":"
+               txtType=$txtType$colType":"
+               fi  
+               ((count++))
+        done
+        touch $nameTable
+        echo Table is added successfully
+        echo "$txtType"
+        printf "$txt\n$txtType\n" > $nameTable  
+         
          ;;
          
          [0-9]*)
@@ -18,11 +47,22 @@ if [ ! -f "$nameTable" ]; then
          echo Incorrect Table name
          ;;
          esac
-         
 elif [[ $nameTable = "" ]]; then
    echo You didnt enter any value
-   
-else 
-   echo Invalid Input Database already exists
-   
+else
+    echo Table doesnt exist
 fi
+
+
+
+          
+          
+          
+          
+          
+          
+          
+          
+          
+      
+      
