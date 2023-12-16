5e0fec12e16e0c7ec98644a15687840b2a7a6274
sara-aref <sara.aref.hassan@gmail.com>
2023-12-16 22:35:07 +0200

    check tables inserttable.sh

--- inserttable.sh
+++ inserttable.sh
@@ -4,48 +4,50 @@ export LC_COLLATE=C
 
 read -p "Enter table name: " name
 
-colnum=$(awk -F: '{if (NR==1) print NF}' "$name")
+if [ ! -f $name ]; then
+	echo "Error! there is no table with that name."
+else
 
-continue=true
-while [[ "$continue" == true ]]
-do
-	fcoltype=$(awk -F: '{if (NR==2) {print $1}}' "$name")
-	if [[ "$fcoltype" == "int" ]]; then
-		read -p "Enter data of row: " datacon
+	colnum=$(awk -F: '{if (NR==1) print NF}' "$name")
 
-		if [[ "$datacon" =~ ^[0-9]+$ ]]; then
-			echo -n $datacon":" >> "$name"
-		fi
-
-	elif [[ "$fcoltype" == "string" ]]; then
-		read -p "Enter data of row: " datacon
-
-		if [[ "$datacon" =~ ^[a-zA-Z0-9]+$ ]]; then
-			echo -n $datacon":" >> "$name"
-		fi
-	fi
-	
-	datatype=$(awk -F: '{if (NR==2) for(i=2;i<=NF;i++) {print $i}}' "$name")
-	for data in $datatype
+	continue=true
+	while [[ "$continue" == true ]]
 	do
-		if [[ "$data" == "int" ]]; then
+		fcoltype=$(awk -F: '{if (NR==2) {print $1}}' "$name")
+		if [[ "$fcoltype" == "int" ]]; then
 			read -p "Enter data of row: " datacon
 
-			if [[ "$datacon" =~ ^[0-9]*$ ]]; then
+			if [[ "$datacon" =~ ^[0-9]+$ ]]; then
 				echo -n $datacon":" >> "$name"
 			fi
 
-		elif [[ "$data" == "string" ]]; then
+		elif [[ "$fcoltype" == "string" ]]; then
 			read -p "Enter data of row: " datacon
 
-			if [[ "$datacon" =~ ^[a-zA-Z0-9]*$ ]]; then
+			if [[ "$datacon" =~ ^[a-zA-Z0-9]+$ ]]; then
 				echo -n $datacon":" >> "$name"
 			fi
 		fi
+	
+		datatype=$(awk -F: '{if (NR==2) for(i=2;i<=NF;i++) {print $i}}' "$name")
+		for data in $datatype
+		do
+			if [[ "$data" == "int" ]]; then
+				read -p "Enter data of row: " datacon
+
+				if [[ "$datacon" =~ ^[0-9]*$ ]]; then
+					echo -n $datacon":" >> "$name"
+				fi
+
+			elif [[ "$data" == "string" ]]; then
+				read -p "Enter data of row: " datacon
+
+				if [[ "$datacon" =~ ^[a-zA-Z0-9]*$ ]]; then
+					echo -n $datacon":" >> "$name"
+				fi
+			fi
+		done
+		echo -e >> "$name"
+		continue=false
 	done
-	echo -e >> "$name"
-	continue=false
-done
-
-
-
+fi
