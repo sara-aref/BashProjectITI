176a953ecbf21c0ee2bb7a91074a4e2efe52fda8
omniagad25 <omniagad25@gmail.com>
2023-12-16 12:55:59 +0200

    createTable file

---
+++ createTable.sh
@@ -0,0 +1,28 @@
+#! /usr/bin/bash
+
+shopt -s extglob #turn on pattern and enable it
+
+read -p "Enter the Table name: .. It shouldn't start with number and must not include any special character or space: " nameTable
+if [ ! -f "$nameTable" ]; then
+
+    case $nameTable in
+         [a-zA-Z_]*)
+         touch $nameTable
+         ;;
+         
+         [0-9]*)
+         echo Incorrect Input remove the number at the beginning
+         ;;
+         
+         *)
+         echo Incorrect Table name
+         ;;
+         esac
+         
+elif [[ $nameDB = "" ]]; then
+   echo You didnt enter any value
+   
+else 
+   echo Invalid Input Database already exists
+   
+fi
