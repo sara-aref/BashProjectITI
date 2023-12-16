860273daa7a0b2e53abc84f3e585fa9cbe3f9782
sara-aref <sara.aref.hassan@gmail.com>
2023-12-16 22:01:44 +0200

    edit inserttable.sh

--- inserttable.sh
+++ inserttable.sh
@@ -6,20 +6,37 @@ read -p "Enter table name: " name
 
 colnum=$(awk -F: '{if (NR==1) print NF}' "$name")
 
-for ((i = 1; i <= $colnum; i++))
+continue=true
+while [[ "$continue" == true ]]
 do
-	datatype=$(awk -F: '{if (NR==2) for(i=1;i<=NF;i++) {print $i}}' "$name")
+	fcoltype=$(awk -F: '{if (NR==2) {print $1}}' "$name")
+	if [[ "$fcoltype" == "int" ]]; then
+		read -p "Enter data of row: " datacon
+
+		if [[ "$datacon" =~ ^[0-9]+$ ]]; then
+			echo -n $datacon":" >> "$name"
+		fi
+
+	elif [[ "$fcoltype" == "string" ]]; then
+		read -p "Enter data of row: " datacon
+
+		if [[ "$datacon" =~ ^[a-zA-Z0-9]+$ ]]; then
+			echo -n $datacon":" >> "$name"
+		fi
+	fi
+	
+	datatype=$(awk -F: '{if (NR==2) for(i=2;i<=NF;i++) {print $i}}' "$name")
 	for data in $datatype
 	do
 		if [[ "$data" == "int" ]]; then
-			read -p "Enter data of row $i: " datacon
+			read -p "Enter data of row: " datacon
 
 			if [[ "$datacon" =~ ^[0-9]*$ ]]; then
 				echo -n $datacon":" >> "$name"
 			fi
 
 		elif [[ "$data" == "string" ]]; then
-			read -p "Enter data of row $i: " datacon
+			read -p "Enter data of row: " datacon
 
 			if [[ "$datacon" =~ ^[a-zA-Z0-9]*$ ]]; then
 				echo -n $datacon":" >> "$name"
@@ -27,6 +44,7 @@ do
 		fi
 	done
 	echo -e >> "$name"
+	continue=false
 done
 
 
