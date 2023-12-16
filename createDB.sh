d571ab5b81e30107973472f9ffe385f5d1c8a06f
omniagad25 <omniagad25@gmail.com>
2023-12-16 07:57:50 +0200

    update createDB

--- createDB.sh
+++ createDB.sh
@@ -2,24 +2,33 @@
 
 shopt -s extglob #turn on pattern and enable it
 
+#nameDB=$(echo "database/$nameDB" | tr ' ' '_')
+#mkdir database/$nameDB
+#echo nameDB
+
 read -p "Enter the Database name: .. It shouldn't start with number and must not include any special character or space: " nameDB
 
-case $nameDB in
+if [ ! -d database/"$nameDB" ]; then
 
-[a-zA-Z_]*)
-mkdir database/$nameDB
-;;
-
-[0-9]*)
-echo Incorrect Input remove the number at the beginning
-;;
-[*" "*])
-nameDB=$(echo "database/$nameDB" | tr ' ' '_')
-mkdir database/$nameDB
-echo nameDB
-;;
-
-*)
-echo Incorrect DB name
-;;
-esac 
+    case $nameDB in
+         [a-zA-Z_]*)
+         mkdir database/$nameDB
+         ;;
+         
+         [0-9]*)
+         echo Incorrect Input remove the number at the beginning
+         ;;
+         
+         [*" "*])
+         newDB=$(echo "database/$nameDB" | tr ' ' '_')
+         mv "database/$nameDB" "$newDB"
+         ;;
+         
+         *)
+         echo Incorrect DB name
+         ;;
+         esac
+   echo Invalid Input Database already exists
+elif [[ $nameDB = "" ]]; then
+   echo You didnt enter any value
+fi
