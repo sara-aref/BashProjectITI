2e2fc24c8631487a95d7b8d4f102634c7c0f819c
omniagad25 <omniagad25@gmail.com>
2023-12-17 23:03:20 +0200

    drop table

--- droptable.sh
+++ droptable.sh
@@ -1,17 +1,10 @@
 #! /bin/bash
 
-shopt -s extglob #turn on pattern and enable it
-
-read -p "Enter Table name you want to connect: " name
+read -p "Enter Table name you want to delete: " name
 
 if [ ! -f "$name" ]; then
 	echo "Error! No Table with that name."
-	
-elif [[ $name == "" ]]; then
-   echo You didnt enter any value
-	
 else
 	rm "$name"
 	echo "Table is deleted"
 fi
-
