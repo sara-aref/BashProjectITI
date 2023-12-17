c3961a9869911c882592c839cea6d62cab6d7b43
omniagad25 <omniagad25@gmail.com>
2023-12-17 23:00:05 +0200

    DB files

---
+++ connectdb.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+read -p "Enter DataBase name you want to connect: " name
+
+if [ ! -d "$name" ]; then
+	echo "Error! No DataBase with that name."
+else
+	cd `pwd`/"$name"
+
+	source ../../table.sh
+fi
+
---
+++ db.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+dbname=databaseso
+if [ -d $dbname ]; then
+	pwd
+else
+	mkdir `pwd`/$dbname
+fi
+
+cd `pwd`/$dbname
+
+select db in createdb listdb dropdb connectdb
+do      
+        case $db in
+        "createdb")
+        	source ../create.sh
+        ;;
+        "listdb")
+        	source ../listdb.sh
+        ;;
+        "dropdb")
+        	source ../dropDB.sh
+        ;;
+        "connectdb")
+        	source ../connectdb.sh
+        ;;
+        *)
+        	echo "Choose from 1 to 4"
+        ;;
+        esac
+done
+
+cd ..
