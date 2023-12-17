ad1f5eeee311e6a3fb4bc129489bd53894a206f9
sara-aref <sara.aref.hassan@gmail.com>
2023-12-18 01:29:24 +0200

    edit dropDB.sh droptable.sh

--- dropDB.sh
+++ dropDB.sh
@@ -2,11 +2,11 @@
 
 shopt -s extglob #turn on pattern and enable it
 
-read -p "Enter the Database name you want to drop " nameDB
+read -p "Enter the Database name you want to drop: " nameDB
 
 
-if [ -d database/"$nameDB" ]; then
-    rm -r database/"$nameDB"
+if [ -d "$nameDB" ]; then
+    rm -r "$nameDB"
     echo "Directory deleted successfully."
     
 elif [[ $nameDB == "" ]]; then
