48d31143a871014313b1629a2f7614c547d0ce83
sara-aref <sara.aref.hassan@gmail.com>
2023-12-18 01:22:35 +0200

    edit createDB

--- createDB.sh
+++ createDB.sh
@@ -5,15 +5,16 @@ shopt -s extglob #turn on pattern and enable it
 
 read -p "Enter the Database name: .. It shouldn't start with number and must not include any special character or space: " nameDB
 
-if [ ! -d database/"$nameDB" ]; then
+if [ ! -d "$nameDB" ]; then
 
     case $nameDB in
          *['!''?'@\#\$%^\&*()-+\.\/';']*)
          echo Incorrect DB name
          ;;
          [a-zA-Z_]*)
-         nameDB=$(echo "databaseso/$nameDB" | tr ' ' '_')
+         nameDB=$(echo "$nameDB" | tr ' ' '_')
          mkdir $nameDB
+         echo "Database created"
          ;;
          [0-9]*)
          echo Incorrect Input remove the number at the beginning
