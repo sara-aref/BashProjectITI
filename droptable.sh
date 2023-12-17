9b10ff0700d2069722ab0d3d09ce756a109f2486
omniagad25 <omniagad25@gmail.com>
2023-12-17 21:49:25 +0200

    omnia file

--- droptable.sh
+++ droptable.sh
@@ -1,10 +1,17 @@
 #! /bin/bash
 
+shopt -s extglob #turn on pattern and enable it
+
 read -p "Enter Table name you want to connect: " name
 
 if [ ! -f "$name" ]; then
 	echo "Error! No Table with that name."
+	
+elif [[ $name == "" ]]; then
+   echo You didnt enter any value
+	
 else
 	rm "$name"
 	echo "Table is deleted"
 fi
+
--- listDB.sh
+++ listDB.sh
@@ -2,7 +2,5 @@
 
 shopt -s extglob #turn on pattern and enable it
 
-echo Here is the table content:
-
 ls -l
 
